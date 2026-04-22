efda324f4e7d3e771351c3d9a512661e02df99cb
Nick Segui <nicksegui5@gmail.com>
2026-04-22 10:08:04 -0500

    final commit

--- prescribers_project.sql
+++ prescribers_project.sql
@@ -1,59 +1,77 @@
 --Q1a
 
-SELECT prescriber.npi, total_claim_count
-FROM prescriber 
-INNER JOIN prescription ON prescriber.npi = prescription.npi
-WHERE total_claim_count IS NOT NULL
-ORDER BY total_claim_count DESC
+SELECT npi, SUM(total_claim_count) AS total_claims
+FROM prescription
+GROUP BY npi
+ORDER BY total_claims DESC
 LIMIT 1;
 
 --Q1b
 
-SELECT nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, total_claim_count
-FROM prescriber 
-INNER JOIN prescription ON prescriber.npi = prescription.npi
+SELECT nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, SUM(total_claim_count) AS total_claims
+FROM prescription
+INNER JOIN prescriber USING (npi)
 WHERE total_claim_count IS NOT NULL
-ORDER BY total_claim_count DESC
+GROUP BY npi, nppes_provider_first_name, nppes_provider_last_org_name, specialty_description
+ORDER BY total_claims DESC
 LIMIT 1;
 
 --Q2a
 
-SELECT specialty_description, total_claim_count
+SELECT specialty_description, SUM(total_claim_count) AS total_claims
 FROM prescriber 
-INNER JOIN prescription ON prescriber.npi = prescription.npi
-WHERE total_claim_count IS NOT NULL
-ORDER BY total_claim_count DESC
+INNER JOIN prescription USING (npi)
+GROUP BY specialty_description
+ORDER BY total_claims DESC
 LIMIT 1;
 
 --Q2b
 
-SELECT specialty_description, total_claim_count
+SELECT specialty_description, SUM(total_claim_count) AS total_claims
 FROM prescriber 
-INNER JOIN prescription ON prescriber.npi = prescription.npi
-INNER JOIN drug ON prescription.drug_name = drug.drug_name
-WHERE total_claim_count IS NOT NULL
-AND opioid_drug_flag = 'Y'
-ORDER BY total_claim_count DESC
+INNER JOIN prescription USING (npi)
+INNER JOIN drug USING (drug_name)
+WHERE opioid_drug_flag = 'Y'
+GROUP BY specialty_description
+ORDER BY total_claims DESC
 LIMIT 1;
 
 --Q2c
 
+SELECT DISTINCT specialty_description
+FROM prescriber
+EXCEPT
+SELECT DISTINCT specialty_description
+FROM prescription
+INNER JOIN prescriber USING (npi);
+
 --Q2d
+
+SELECT specialty_description, 
+SUM(CASE WHEN opioid_drug_flag = 'Y' THEN total_claim_count ELSE 0 END) AS opioid_claims,
+SUM(total_claim_count) AS total_claims,
+ROUND(SUM(CASE WHEN opioid_drug_flag = 'Y' THEN total_claim_count ELSE 0 END)/ SUM(total_claim_count) * 100, 2) AS opioid_percentage
+FROM prescriber
+INNER JOIN prescription USING (npi)
+INNER JOIN drug USING (drug_name)
+GROUP BY specialty_description;
 
 --Q3a
 
-SELECT generic_name, total_drug_cost
+SELECT generic_name, SUM(total_drug_cost) AS total_cost
 FROM drug
-INNER JOIN prescription ON prescription.drug_name = drug.drug_name
-ORDER BY total_drug_cost DESC
+INNER JOIN prescription USING (drug_name)
+GROUP BY generic_name
+ORDER BY total_cost DESC
 LIMIT 1;
 
 --Q3b
 
-SELECT generic_name, ROUND(total_drug_cost/total_day_supply, 2) AS cost_per_day
+SELECT generic_name, ROUND(SUM(total_drug_cost)/ SUM(total_day_supply), 2) AS total_cost_per_day
 FROM drug
-INNER JOIN prescription ON prescription.drug_name = drug.drug_name
-ORDER BY cost_per_day DESC
+INNER JOIN prescription USING (drug_name)
+GROUP BY generic_name
+ORDER BY total_cost_per_day DESC
 LIMIT 1;
 
 --Q4a
@@ -68,28 +86,40 @@ FROM drug;
 
 --Q4b
 
-SELECT drug.drug_name, total_drug_cost::money,
+SELECT SUM(total_drug_cost::money) AS sum_cost,
 CASE
 WHEN opioid_drug_flag = 'Y' THEN 'opioid'
 WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
 ELSE 'neither'
 END AS drug_type
 FROM drug
-INNER JOIN prescription ON prescription.drug_name = drug.drug_name;
+INNER JOIN prescription USING (drug_name)
+GROUP BY drug_type
+ORDER BY sum_cost DESC;
 
 --Q5a
 
-SELECT COUNT(DISTINCT cbsa)
+SELECT COUNT (DISTINCT cbsa)
 FROM cbsa
-WHERE cbsaname LIKE '%TN%';
+INNER JOIN fips_county USING (fipscounty)
+WHERE state = 'TN';
 
 --Q5b
 
-SELECT *
+SELECT cbsaname, SUM(population) AS total_pop
 FROM cbsa
-INNER JOIN population ON cbsa.fipscounty = population.fipscounty;
+INNER JOIN population USING (fipscounty)
+GROUP BY cbsaname
+ORDER BY total_pop DESC;
 
 --Q5c
+
+SELECT county, population
+FROM fips_county
+FULL JOIN cbsa USING (fipscounty)
+LEFT JOIN population  USING (fipscounty)
+WHERE cbsa IS NULL
+ORDER BY population DESC NULLS LAST;
 
 --Q6a
 
@@ -114,8 +144,29 @@ WHERE total_claim_count >= 3000;
 
 --Q7a
 
-SELECT prescriber.npi
+SELECT npi, drug_name
 FROM prescriber
 CROSS JOIN drug
 WHERE specialty_description = 'Pain Management'
-AND nppes_provider_city = 'Nashville'
+AND nppes_provider_city = 'NASHVILLE'
+AND opioid_drug_flag = 'Y';
+
+--Q7b
+
+SELECT npi, drug_name, total_claim_count
+FROM prescriber
+CROSS JOIN drug
+LEFT JOIN prescription USING (npi, drug_name)
+WHERE specialty_description = 'Pain Management'
+AND nppes_provider_city = 'NASHVILLE'
+AND opioid_drug_flag = 'Y';
+
+--Q7c
+
+SELECT npi, drug_name, COALESCE(total_claim_count, 0)
+FROM prescriber
+CROSS JOIN drug
+LEFT JOIN prescription USING (npi, drug_name)
+WHERE specialty_description = 'Pain Management'
+AND nppes_provider_city = 'NASHVILLE'
+AND opioid_drug_flag = 'Y'
